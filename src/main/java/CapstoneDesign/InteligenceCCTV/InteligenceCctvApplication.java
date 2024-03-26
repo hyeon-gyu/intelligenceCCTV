cbce59ee2bdb3097581854173b496efb8b94ed51
hyeon-gyu <hyungue729@naver.com>
2024-03-26 18:27:19 +0900

    #1 environment setting

---
+++ src/main/java/CapstoneDesign/InteligenceCCTV/InteligenceCctvApplication.java
@@ -0,0 +1,13 @@
+package CapstoneDesign.InteligenceCCTV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteligenceCctvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteligenceCctvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CapstoneDesign/InteligenceCCTV/InteligenceCctvApplicationTests.java
@@ -0,0 +1,13 @@
+package CapstoneDesign.InteligenceCCTV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteligenceCctvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
